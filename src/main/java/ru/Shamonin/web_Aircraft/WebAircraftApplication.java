bd4ed90f19531b423ca1b5e8a123dae517cca53e
Phil-Shepard <Philipp-74@yandex.ru>
2023-04-25 16:51:07 +0500

    Initial commit

---
+++ src/main/java/ru/Shamonin/web_Aircraft/WebAircraftApplication.java
@@ -0,0 +1,13 @@
+package ru.Shamonin.web_Aircraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAircraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAircraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/Shamonin/web_Aircraft/WebAircraftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Shamonin.web_Aircraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAircraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
